b279ae220a519da8a3fb07348f51a4c51a52fc5e
Mrudul Kolambe <mrudulkolambe02@gmail.com>
2023-12-05 00:01:18 +0530

    changes

---
+++ src/app/login/page.jsx
@@ -0,0 +1,13 @@
+"use client";
+
+import React from 'react'
+
+const LoginPage = () => {
+  return (
+	<>
+	
+	</>
+  )
+}
+
+export default LoginPage
---
+++ src/context/Auth.jsx
@@ -0,0 +1,24 @@
+const { createContext, useContext, useState, useEffect } = require("react");
+
+
+const AuthContext = createContext();
+
+const AuthContextProvider = ({ children }) => {
+	const [token, setToken] = useState("");
+	const [user, setUser] = useState();
+	useEffect(() => {
+		if (token) {
+
+		}
+	}, [token])
+
+	return <AuthContext.Provider value={{ user, token, setToken }}>
+		{children}
+	</AuthContext.Provider>
+}
+
+const UseAuthContext = () => {
+	return useContext(AuthContext);
+}
+
+export { AuthContextProvider, UseAuthContext }
