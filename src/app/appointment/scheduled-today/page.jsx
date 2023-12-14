56069345e226c2388c6d0bd315334580dcb9f250
Prachi More <prachimore2702@gmail.com>
2023-12-15 01:04:53 +0530

    get appointments done

--- src/app/appointment/scheduled-today/page.jsx
+++ src/app/appointment/scheduled-today/page.jsx
@@ -1,8 +1,23 @@
+"use client"
+
 import Button from '@/components/Button'
 import Layout from '@/components/Layout'
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import axios from 'axios'
 
 const page = () => {
+
+  const [appointments, setAppointments] = useState([])
+  useEffect(() => {
+    axios('https://highland-hospital-backend.vercel.app/get-appointment', {
+      method: 'GET',
+      headers: {"Authorization": "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6ImFkbWluIiwicGFzc3dvcmQiOiIkMmIkMTAkWk9DZnVJYkQ4ZHhnMFI3MjVsMzlUT0tNYVJwY3dRMzNQZW5UQkdQYWdnY3M1bDFtL1ZZcWEiLCJpYXQiOjE3MDI1NTM2NDd9.e88TIYPxwjcLVAe0Q4dy0Ep0UEigbFJQy6bODbQ0Cbw"}
+    })
+      .then((res) => {
+        setAppointments(res.data.response)
+      })
+  }, [])
+
   return (
     <>
       <Layout>
@@ -32,7 +47,23 @@ const page = () => {
                     </tr>
                   </thead>
                   <tbody className='text-gray-800 text-sm'>
-                    <tr className="bg-white border-b">
+                    {appointments?.map((value, index) => {
+                      return <tr className="bg-white border-b">
+                        <td className="px-6 py-4">1</td>
+                        <td className="px-6 py-4">{value.firstname}</td>
+                        <td className="px-6 py-4">{value.doctor}</td>
+                        <td className="px-6 py-4">{value.dateofAppointment}</td>
+                        <td className="px-6 py-4">{value.timeofAppointment}</td>
+                        <td className="px-6 py-4">{value.phoneNumber}</td>
+                        <td className="px-6 py-4">{value.status}</td>
+                        <td className="px-6 py-4">action 1</td>
+                        <td className="px-6 py-4">action 2</td>
+                        <td className="px-6 py-4">action 3</td>
+                      </tr>
+                    })
+                    }
+
+                    {/* <tr className="bg-white">
                       <td className="px-6 py-4">1</td>
                       <td className="px-6 py-4">Prachi</td>
                       <td className="px-6 py-4">doctor</td>
@@ -43,19 +74,7 @@ const page = () => {
                       <td className="px-6 py-4">action 1</td>
                       <td className="px-6 py-4">action 2</td>
                       <td className="px-6 py-4">action 3</td>
-                    </tr>
-                    <tr className="bg-white">
-                      <td className="px-6 py-4">1</td>
-                      <td className="px-6 py-4">Prachi</td>
-                      <td className="px-6 py-4">doctor</td>
-                      <td className="px-6 py-4">12/12/23</td>
-                      <td className="px-6 py-4">12:30</td>
-                      <td className="px-6 py-4">1234567890</td>
-                      <td className="px-6 py-4">status</td>
-                      <td className="px-6 py-4">action 1</td>
-                      <td className="px-6 py-4">action 2</td>
-                      <td className="px-6 py-4">action 3</td>
-                    </tr>
+                    </tr> */}
                   </tbody>
                 </table>
               </div>
--- src/context/Auth.jsx
+++ src/context/Auth.jsx
@@ -1,3 +1,5 @@
+"use client"
+
 const { createContext, useContext, useState, useEffect } = require("react");
 
 
